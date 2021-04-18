89d574c5da50a585dcd66f1a46236c364a8f7879
snelpatel <snelpatel@gmail.com>
2021-04-18 10:07:17 +0530

    Profile page added with its styles

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {Route, Switch} from "react-router-dom"; // For redirection
 import Login from "./screens/login/Login";
 import Home from "./screens/home/Home";
-//import Profile from "./screens/profile/Profile";
+import Profile from "./screens/profile/Profile";
 
 /**
  * This class is built entire Application.
@@ -13,7 +13,7 @@ class ImageViewer extends Component {
             <Switch>
                 <Route exact path='/' render={(props) => <Login {...props}/>}/>
                 <Route exact path='/home' render={(props) => <Home {...props}/>}/>
-               
+                <Route exact path='/profile' render={(props) => <Profile {...props}/>}/>
             </Switch>
         </Fragment>
     }
